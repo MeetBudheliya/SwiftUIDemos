f1d88f296aa2fc062d2adfbfdcf2c8d92e8635a3
Meet Budheliya <Meetbudheliya04@gmail.com>
2021-04-20 16:02:15 +0530

    Initial Commit

---
+++ SwiftUIProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIProject
+//
+//  Created by Adsum MAC 1 on 20/04/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
